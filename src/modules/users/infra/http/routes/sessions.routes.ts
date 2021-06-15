dd53554a2748bd2143bfd96e8ce4bece84fa4a9b
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-06-15 15:59:17 -0300

    feat(authentication): Exposed the authentication route
    Created the session controller and the sessions.routes

---
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,24 @@
+import { celebrate, Joi, Segments } from "celebrate";
+import { Router } from "express";
+
+import SessionsController from "../controllers/SessionsController";
+
+const controller = new SessionsController();
+
+const sessionRouter = Router();
+
+sessionRouter.post(
+  "/",
+
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+
+      password: Joi.string().required(),
+    },
+  }),
+
+  controller.create
+);
+
+export default sessionRouter;
