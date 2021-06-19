b01838ba3184c95c1aa960b82bfb797be93e90ac
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-06-19 19:40:50 -0300

    feat(order): New order send message to customer
    Now every time an new order is created a message is sent to the customer

--- src/modules/orders/infra/http/controllers/OrderController.ts
+++ src/modules/orders/infra/http/controllers/OrderController.ts
@@ -1,3 +1,4 @@
+import SendMessageService from "@modules/messaging/services/SendMessageService";
 import CreateOrderService from "@modules/orders/services/CreateOrderService";
 import ListOrderService from "@modules/orders/services/ListOrderService";
 import { classToClass } from "class-transformer";
@@ -17,6 +18,12 @@ export default class OrderController {
       productName,
       stageList,
     });
+
+    const sendMessageService = container.resolve(SendMessageService);
+
+    const body = `Olá ${order.clientName}, seu pedido de id ${order.id} foi registrado a primeira etapa é ${order.stages[0].name} acesse o link testeUrl para visualizar seu pedido`;
+
+    await sendMessageService.execute({ to: order.clientPhone, from: "+14155238886", body });
 
     return response.status(201).json(classToClass(order));
   }
